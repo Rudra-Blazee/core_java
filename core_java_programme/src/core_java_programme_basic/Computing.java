411df95e43620a91175d3b66adf4bb24af3a6d08
sumit <sumit@sumit>
2020-08-09 08:24:51 +0530

    commit

---
+++ core_java_programme/src/core_java_programme_basic/Computing.java
@@ -0,0 +1,17 @@
+package core_java_programme_basic;
+
+import java.util.Scanner;
+
+
+public class Computing 
+{
+public static void main(String[] args) 
+	{
+	Scanner sc=new Scanner(System.in);
+	System.out.println("enter two number");
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	int c=a+b;
+	System.out.println("Sum of two no is "+c);
+	}
+}
---
+++ core_java_programme/src/core_java_programme_basic/Helloworld.java
@@ -0,0 +1,10 @@
+package core_java_programme_basic;
+
+public class Helloworld 
+{
+ public static void main (String[] args)  
+ {
+	System.out.print("Helloworld");
+	System.
+ }
+}
