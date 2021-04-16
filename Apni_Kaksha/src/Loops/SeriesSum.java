353c9e44191160f21063b5e3ad60016263f560fe
sumit <sumit@Blazee>
2021-04-17 00:46:52 +0530

    eclipse push

--- Apni_Kaksha/src/Loops/SeriesSum.java
+++ Apni_Kaksha/src/Loops/SeriesSum.java
@@ -6,14 +6,13 @@ public class SeriesSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner s= new Scanner(System.in);
-		int t=s.nextInt();
-		 float result=0;
-		 for(float i =1;i<=t;i++)
-		 {
-			 result += 1/i;
-		 }
-		 System.out.println(result);
+		Scanner s = new Scanner(System.in);
+		int t = s.nextInt();
+		float result = 0;
+		for (float i = 1; i <= t; i++) {
+			result += 1 / i;
+		}
+		System.out.println(result);
 	}
 
 }
--- Apni_Kaksha/src/Strings/ArrayFunctions.java
+++ Apni_Kaksha/src/Strings/ArrayFunctions.java
@@ -2,23 +2,22 @@ package Strings;
 
 public class ArrayFunctions {
     public static void main(String[] args) {
-        // String name = "Sumit Anand";
-        // String name1 = new String("Sumit Anand");
-        // String name2 = "";
-        // System.out.println(name.charAt(0));
-        // System.out.println(name.length());
-        // System.out.println(name.substring(4));
-        // System.out.println(name.substring(4, 8));
-        // System.out.println(name.contains("Sumit"));
-        // System.out.println(name.equals(name1));
-        // System.out.println(name2.isEmpty());
-        // System.out.println(name.concat(" kumar"));
-        // System.out.println(name.replace('A', 'C'));
+         String name = "Sumit Anand";
+         String name1 = new String("Sumit Anand");
+         String name2 = "";
+         System.out.println(name.charAt(0));
+         System.out.println(name.length());
+         System.out.println(name.substring(4));
+         System.out.println(name.substring(4, 8));
+         System.out.println(name.contains("Sumit"));
+         System.out.println(name.equals(name1));
+         System.out.println(name2.isEmpty());
+         System.out.println(name.concat(" kumar"));
+         System.out.println(name.replace('A', 'C'));
         String cars = "Hundai,Maruti,Swift,Wagonr";
         String allcars[] = cars.split(",");
         for (String e : allcars) {
             System.out.print(e);
         }
-
     }
 }
